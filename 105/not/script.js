1a62c5ec919958f2133fbcffc3bc824318b5c1d4
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-02-15 11:35:42 -0500

    update

--- 105/not/script.js
+++ 105/not/script.js
@@ -12,24 +12,38 @@ $(document).ready(function () {
     }
   });
 
-  function showNotification() {
+  function showNotification(type) {
     if (Notification.permission !== 'granted') {
       Notification.requestPermission();
-    } else {
-      const options = {
-        body: 'New Update', //Title
+    } else if (type === "game") {
+      const game = {
+        requireInteraction: 'true',
+        tag: 'Sebastian-105',
+        title: 'New Game',
+        body: 'Theres a new game to play.',
         dir: 'ltr',
-        icon: '/105.png',
-        image: '/105.png'
-        
+        icon: '/105.png'
+      }; const notification = new Notification('Notification', game);
+    } else if (type === "update") {
+      const update = {
+          requireInteraction: 'true',
+          tag: 'Sebastian-105',
+          title: 'New Update',
+          body: 'Theres a new update available.',
+          dir: 'ltr',
+          icon: '/105.png'
       };
-      const notification = new Notification('Notification', options);
-
+      const notification = new Notification('Notification', update);} else {
+        const update = {
+          requireInteraction: 'true',
+          tag: 'Sebastian-105',
+          title: 'Sebastian-105',
+          body: 'empty',
+          dir: 'ltr',
+          icon: '/105.png'
+      };       const notification = new Notification('Notification', update);}
       notification.onclick = function () {
         window.open('https://github.com/Sebastian-105/Sebastian-105.github.io/blob/Sebastian-105/CHANGELOG.md');
       };
     }
-  }
-
-  $('#btn-show-notification').on('click', showNotification);
-});
+  } .$('#btn-show-notification').on('click', showNotification))
