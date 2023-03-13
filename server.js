6abf5566dd6ac578eef479334875f660baa7f886
Sebastian-105 <28schapfel1@gmail.com>
2023-03-13 13:39:14 +0000

    Update package.json and server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var ip = require('ip');
 console.log(ip.address());
 var app = express();
-app.use(express.static('/'));
+app.use(express.static('public'));
 app.listen(8080);
