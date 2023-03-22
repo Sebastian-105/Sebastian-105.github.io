34764302363bf996b8af97df1d879595ac29f733
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-03-22 14:22:46 -0400

    update

--- assets/js/Sebastian-105.js
+++ assets/js/Sebastian-105.js
@@ -17,6 +17,8 @@ Made by @Sebastian-105 and by @DanyangXu77.
 ───────────────────────────────────────────────────────────────────────────────────────────────────|
 ───────────────────────────────────────────────────────────────────────────────────────────────────|-->
 */
+localStorage.setItem('tabicon105', '/105.png');
+localStorage.setItem('tabname1234', 'Sebastian-105')
 function Sebastian105() {
 	window.addEventListener('offline', () => 
   $("offline").fadeIn("slow"))
@@ -26,6 +28,8 @@ function closesidenav() {
   $(".sidenav").fadeToggle("slow");
   console.log('Closed sidenav');
 }
+var examplelink = location.href
+
 var author = "Sebastian-105";
 
 function copytoclipboard(copiedcontent){
@@ -33,9 +37,6 @@ function copytoclipboard(copiedcontent){
   console.log("Username has been copied.");
   $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
   document.getElementById("copyalertcorner").innerHTML = "Successfully copied: " + copiedcontent;
-}
-function MyFunctionalert(){
-  return "Visit my website!";
 }
 function settings() {
   if (confirm("This page is still a work in progress, are you sure you want to go here? \n-Sebastian-105") === true) {
@@ -70,55 +71,8 @@ function checkTimems(i) {
   if (i <100) {i = "0" + i}
   return i;
 }
-
-setTimeout(theexacttime,105);
-function myFunctionsearch() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("mySearch");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}/*--------------------------------------------------------------------------------*/
-function showResult(str) {
-  if (str.length==0) {
-    document.getElementById("livesearch").innerHTML="";
-    document.getElementById("livesearch").style.border="0px";
-    return;
-  }
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("livesearch").innerHTML=this.responseText;
-      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-    }
-  };
-  xmlhttp.open("GET","livesearch.php?q="+str,true);
-  xmlhttp.send();
-}
 // Varibles
 //Sidenav
-		if ('scrollRestoration' in history) {
-		  history.scrollRestoration = 'manual';
-;
-		}
-close = document.getElementsByClassName("closealert");
-var i;
-
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function(){
-    var div = this.parentElement;
-    div.style.opacity = "0";
-    setTimeout(function(){ div.style.display = "none"; }, 600);
-  }
-}
 //FUNCTIONS
      function blocked() {
   var myWindow = window.open("105/blocked/index.html", "_blank", "width=500,height=400,resizable=yes,left=500,top=200,toolbar=yes");
@@ -176,13 +130,6 @@ function myFunctionspotify() {
 function Chatroom() {
   var myWindow = window.open("105/chatroom/index.html", "_blank");
 }
-function onFunction() {
-  alert ("Your browser has connected back to the internet connection. \n-Sebastian-105");
-}
-
-function offFunction() {
-  alert ("We detected that your browser has lost internet connection. \n-Sebastian-105");
-}
 function myfunctionofflinegames() {
   var myWindow = window.open("p/offline/index.html");
 }
@@ -209,55 +156,63 @@ if(Enabled == false)
 function keybinds(){
   var myWindow = window.open("105/keybinds/index.html");
 }
-function tabicon() {
-  var tabiconvar = document.getElementById("tabchange").value;
-  var newtabicon = $("link[rel*='icon']").attr("href", tabiconvar);
-  localStorage.setItem('tabicon', tabiconvar);
-}
 function buttonCode()
 {
-	var textbox = document.getElementById("nameChange")
-  if (textbox.value.includes("Sebastian-105")) {
-    document.title = (textbox.value)
-  } else {
-    newtitle105 = textbox.value + " | Sebastian-105"
-    document.title = (newtitle105)
-  }
-    textbox.value = ""
-    textbox.focus();
+  var tabnameforrealz = prompt("New Tab title:", 'Example: ' + document.title);
+  var tabnameforrealz123 = document.title = tabnameforrealz
+  localStorage.setItem('tabname1234', tabnameforrealz);
+  $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
+  document.getElementById("copyalertcorner").innerHTML = `Successfully Changed tab title to: ${document.title}`;
 }
-if (document.title.includes("Sebastian-105")) {
-} else if (document.title.includes("Sebastian105")) {
-  document.title = "Sebastian-105";
-} else if (document.title = " | Sebastian-105") {
-  document.title = "Sebastian-105"
+function tabicon() {
+  
+  var thetabicon = prompt("URL for image: \nMust include https:// or http://", examplelink);
+  var tabiconforrealz = $("link[rel*='icon']").attr("href", thetabicon);
+ localStorage.setItem('tabicon105', tabiconforrealz);
+ if (thetabicon.includes('https://')  || thetabicon.includes("http://") ||thetabicon.includes("file://") ||thetabicon.includes('localhost')){
+ $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
+  document.getElementById("copyalertcorner").innerHTML = "Successfully Changed tab icon " + thetabicon;
+} else {
+  $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
+  $("copyalertcorner").css('background-color', 'red');
+  document.getElementById("copyalertcorner").innerHTML = "Fail to Change tab icon. Check to see if it includes https://";
 }
-  else {
-  var title = document.title;
-  document.title += " | Sebastian-105";
 }
 function resettabname() {
   document.title = "Sebastian-105";
+  localStorage.getItem("tabname1234", "Sebastian-105");
+  $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
+  document.getElementById("copyalertcorner").innerHTML = `Successfully Reset tab name back to ${document.title}`;
 } 
 document.addEventListener("keydown", e => {
-  e.preventDefault();
-  if (e.key === "f" && e.ctrlKey) {
-    if (!document.fullscreenElement) {
-      document.requestFullscreen();
-    }
-  }
-    else if (e.key === "g" && e.ctrlKey) {
+      if (e.key === "g" && e.ctrlKey) {
+      e.preventDefault();
       console.log("game page opened");
       window.open("game/index.html");
     } else if (e.key === "s" && e.ctrlKey) {
+      e.preventDefault();
       window.open("105/settings/index.html");
       console.log("settings page opened");
     } else if (e.key === "h" && e.ctrlKey) {
+      e.preventDefault();
       window.open("./index.html");
       console.log("settings page opened");
     } else if (e.key === "c" && e.ctrlKey) {
+      e.preventDefault();
       window.open("105/chatroom/index.html");
     } else if (e.key === "d" && e.ctrlKey) {
-      window.open("docs/index.html"); 
-    }
-})
+      e.preventDefault();
+      window.open("docs/index.html"); }
+  });
+  window.addEventListener('online',  online);
+window.addEventListener('offline', offline);
+function online() {
+  $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
+  document.getElementById("copyalertcorner").innerHTML = `You are back Online`;
+
+}
+function offline() {
+  $("copyalertcorner").fadeIn("slow").delay(5000).fadeOut("slow");
+  $('copyalertcorner').addClass('offline');
+  document.getElementById("copyalertcorner").innerHTML = `Offline | Reconnect and try again | Offline`;
+}
