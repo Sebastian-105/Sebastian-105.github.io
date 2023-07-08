d2bc7cc00579f2c60e1e42b9a0d6d076fcf2935c
Sebastian-105 <83544451+Sebastian-105@users.noreply.github.com>
2023-07-08 20:53:21 +0000

    Added public foder

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.use(express.static('./'));
+app.use(express.static('./public'));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
