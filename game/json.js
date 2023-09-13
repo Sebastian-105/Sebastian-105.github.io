d4015c8849617b5fc8dda2c331dfc1ef4e53edc0
Seb_105 <83544451+Sebastian-105@users.noreply.github.com>
2023-09-13 13:00:30 -0400

    a

--- game/json.js
+++ game/json.js
@@ -3,30 +3,44 @@
 // This is a JavaScript code that creates a game catalog page with a search feature, a game detail page, and the ability to save and load user data. It uses fetch to load game data from a JSON file, creates game elements for each game, and adds click event listeners to show the game in a game container. The code also includes functions to handle saving and loading user data as well as a function to handle a specific key sequence.
 
 // Select the elements
-const gamesContainer = document.querySelector('.games');
-const gameContainer = document.querySelector('.gamecontainer');
-const gameFrame = gameContainer.querySelector('.frame');
-const gameNav = gameContainer.querySelector('.nav');
+const gamesContainer = document.querySelector(".games");
+const gameContainer = document.querySelector(".gamecontainer");
+const gameFrame = gameContainer.querySelector(".frame");
+const gameNav = gameContainer.querySelector(".nav");
 
 // Fetch the games data from a JSON file
-fetch('./games.json')
+fetch("./games.json")
   .then((res) => res.json())
   .then((games) => {
     // Loop through each game and create a new game element for it
     games.forEach((game) => {
-      const game105 = document.createElement('div');
-      game105.className = 'game-box';
+      const game105 = document.createElement("div");
+      game105.className = "game-box";
       game105.innerHTML = `<!-- ${game.name} -->
-        <div class="a"><div class="banner" style='background-image:var(--)'><seb><aclass="classText" href="${game.root}/" style="color:white;" target="_blank" rel="noopener noreferrer" onclick='console.log("Clicked on ${game.name}")'><div class="main">${game.name}</div><div class="sub">${game.sub}</div></a></div><div class="aboutthis">?</div><div></div></seb></div></div>`;
+      <div class="a">
+      <div class="banner">
+         <seb>
+            <aclass="classText" href="${game.root}/" style="color:white;" target="_blank" rel="noopener noreferrer" onclick='console.log("Clicked on ${game.name}")'>
+            <div class="main">${game.name}</div>
+            <div class="sub">${game.sub}</div>
+            </a>
+      </div>
+      <div class="aboutthis" onclick="">?</div><div></div><div class="desc" id="${game.id}">${game.desc}</div></seb>
+   </div>
+   </div>`;
+   game105.onclick = (e) => {
+    let desca = '#'+game.id
+   $(desca).fadeIn(105);
+  // (game.name)
+  };
       gamesContainer.appendChild(game105);
       // Add click event listener to the game element to show the game in the game container
 
       // Add click event listener to the back button in the game container to go back to the games list
-      gameNav.querySelector('#back').addEventListener('click', (e) => {
-      });
+      gameNav.querySelector("#back").addEventListener("click", (e) => {});
 
       // Add click event listener to the fullscreen button in the game container to enter fullscreen mode
-      gameNav.querySelector('#fullscreen').addEventListener('click', (e) => {
+      gameNav.querySelector("#fullscreen").addEventListener("click", (e) => {
         if (!document.fullscreenElement) {
           gameFrame.requestFullscreen();
         }
@@ -34,20 +48,20 @@ fetch('./games.json')
     });
   })
   .catch((e) => {
-    alert('Could not load games\n-Sebastian-105');
+    alert("Could not load games\n-Sebastian-105");
     alert(e + "\n-Sebastian-105");
-    location.replace('./file.html');
+    location.replace("./file.html");
   });
 
 // Hide the spinner element after the page is loaded
-document.querySelector('.spinner').style.display = 'none';
+document.querySelector(".spinner").style.display = "none";
 
 // Function to get the main save data
 function getMainSave() {
   var mainSave = {};
 
   // List of items in localStorage that should not be saved
-  var localStorageDontSave = ['theme', 'tab', 'nebelung'];
+  var localStorageDontSave = ["theme", "tab", "nebelung"];
 
   // Convert localStorage to an array of key-value pairs and remove the items that should not be saved
   localStorageSave = Object.entries(localStorage);
@@ -73,7 +87,7 @@ function getMainSave() {
   mainSave = btoa(JSON.stringify(mainSave));
 
   // Encrypt the mainSave data using AES encryption with the key 'save'
-  mainSave = CryptoJS.AES.encrypt(mainSave, 'save').toString();
+  mainSave = CryptoJS.AES.encrypt(mainSave, "save").toString();
 
   // Return the encrypted mainSave data
   return mainSave;
@@ -84,9 +98,9 @@ function downloadMainSave() {
   var data = new Blob([getMainSave()]);
   var dataURL = URL.createObjectURL(data);
 
-  var fakeElement = document.createElement('a');
+  var fakeElement = document.createElement("a");
   fakeElement.href = dataURL;
-  fakeElement.download = 'games.save';
+  fakeElement.download = "games.save";
   fakeElement.click();
   URL.revokeObjectURL(dataURL);
 }
@@ -94,7 +108,7 @@ function downloadMainSave() {
 // Function to get the main save data from an uploaded file
 function getMainSaveFromUpload(data) {
   // Decrypt the uploaded data using AES decryption with the key 'save'
-  data = CryptoJS.AES.decrypt(data, 'save').toString(CryptoJS.enc.Utf8);
+  data = CryptoJS.AES.decrypt(data, "save").toString(CryptoJS.enc.Utf8);
 
   // Parse the decrypted data as JSON
   var mainSave = JSON.parse(atob(data));
@@ -103,7 +117,10 @@ function getMainSaveFromUpload(data) {
 
   // Set the items in localStorage using the uploaded data
   for (let item in mainLocalStorageSave) {
-    localStorage.setItem(mainLocalStorageSave[item][0], mainLocalStorageSave[item][1]);
+    localStorage.setItem(
+      mainLocalStorageSave[item][0],
+      mainLocalStorageSave[item][1]
+    );
   }
 
   // Set the cookies using the uploaded data
@@ -112,11 +129,11 @@ function getMainSaveFromUpload(data) {
 
 // Function to handle the file upload
 function uploadMainSave() {
-  var hiddenUpload = document.querySelector('.hiddenUpload');
+  var hiddenUpload = document.querySelector(".hiddenUpload");
   hiddenUpload.click();
 
   // Listen for the change event on the file input element
-  hiddenUpload.addEventListener('change', function (e) {
+  hiddenUpload.addEventListener("change", function (e) {
     var files = e.target.files;
     var file = files[0];
     if (!file) {
@@ -130,11 +147,11 @@ function uploadMainSave() {
       getMainSaveFromUpload(e.target.result);
 
       // Show a success message to the user
-      var uploadResult = document.querySelector('.uploadResult');
-      uploadResult.innerText = 'Uploaded save!';
-      uploadResult.style.display = 'initial';
+      var uploadResult = document.querySelector(".uploadResult");
+      uploadResult.innerText = "Uploaded save!";
+      uploadResult.style.display = "initial";
       setTimeout(function () {
-        uploadResult.style.display = 'none';
+        uploadResult.style.display = "none";
       }, 3000);
     };
 
@@ -143,10 +160,10 @@ function uploadMainSave() {
 }
 
 // Handle the hii pattern when keys are pressed
-var hiiPattern = ['h', 'i', 'i'];
+var hiiPattern = ["h", "i", "i"];
 var hiiCurrent = 0;
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener("keydown", function (e) {
   if (e.key !== hiiPattern[hiiCurrent]) {
     return (hiiCurrent = 0);
   }
@@ -155,47 +172,52 @@ document.addEventListener('keydown', function (e) {
 
   if (hiiPattern.length == hiiCurrent) {
     hiiCurrent = 0;
-    document.querySelector('.hii').removeAttribute('hidden');
+    document.querySelector(".hii").removeAttribute("hidden");
   }
 });
- //Shadow on scroll script
-      let lastKnownScrollPosition = 0;
-      let ticking = false;
-      document.addEventListener('scroll', function (e) {
-        lastKnownScrollPosition = window.scrollY;
-        if (!ticking) {
-          window.requestAnimationFrame(function () {
-            if (lastKnownScrollPosition == 0) {
-              document.getElementsByTagName("nav")[0].style.boxShadow = "";
-            } else {
-              document.getElementsByTagName("nav")[0].style.boxShadow = "0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%)";
-            }
-            ticking = false;
-					});
-          ticking = true;
-        }
-      });
+//Shadow on scroll script
+let lastKnownScrollPosition = 0;
+let ticking = false;
+document.addEventListener("scroll", function (e) {
+  lastKnownScrollPosition = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      if (lastKnownScrollPosition == 0) {
+        document.getElementsByTagName("nav")[0].style.boxShadow = "";
+      } else {
+        document.getElementsByTagName("nav")[0].style.boxShadow =
+          "0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%)";
+      }
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
 
-      function myFunction105() {
-        var element = document.body;
-        element.classList.toggle("dark-mode");
-      }
-      
-      function myFunction77() {
-        const banner = document.querySelectorAll('.banner');
-        banner.forEach(banner => {
-          var link = 'background-image: url("../assets/images/banner';
-          let a = (Math.floor(Math.random() * 9)) + 1;
-          link += a.toString();
-          link += '.jpg");';
-          banner.style = link;
-        });
-      }
-      
-      function myFunction2468() {
-        var myWindow = window.open("../index.html", "_blank", "width=400,height=400");
-      }
+function myFunction105() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
-      function blocked2() {
-        var myWindow = window.open("../105/blocked/index.html", "_blank", "width=300,height=330,resizable=yes,left=500,top=200,toolbar=yes");
-      }
+function myFunction77() {
+  const banner = document.querySelectorAll(".banner");
+  banner.forEach((banner) => {
+    var link = 'background-image: url("../assets/images/banner';
+    let a = Math.floor(Math.random() * 9) + 1;
+    link += a.toString();
+    link += '.jpg");';
+    banner.style = link;
+  });
+}
+
+function myFunction2468() {
+  var myWindow = window.open("../index.html", "_blank", "width=400,height=400");
+}
+
+function blocked2() {
+  var myWindow = window.open(
+    "../105/blocked/index.html",
+    "_blank",
+    "width=300,height=330,resizable=yes,left=500,top=200,toolbar=yes"
+  );
+}
