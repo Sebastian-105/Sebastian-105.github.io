63ed919f79ff6fecce738c5bc6161907982c4286
Seb_105 <83544451+Sebastian-105@users.noreply.github.com>
2023-09-14 17:55:58 +0000

    a

---
+++ index.js
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+const express = require('express');
+const http = require('http');
+
+// Constants
+const PORT = 1050;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.use(express.static('./'));
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Node.js is working\n- Sebastian-105`)
