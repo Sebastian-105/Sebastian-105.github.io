f57c713f5d8979802af0c94c3369c1164b1c6fef
Seb_105 <28schapfel1@gmail.com>
2023-10-05 11:03:27 -0400

    asdf

--- game/json.js
+++ game/json.js
@@ -9,12 +9,13 @@ const gameFrame = gameContainer.querySelector(".frame");
 const gameNav = gameContainer.querySelector(".nav");
 
 // Fetch the games data from a JSON file
-fetch("/assets/json/games2.json")
+fetch("./games2.json")
   .then((res) => res.json())
   .then((games) => {
     // Loop through each game and create a new game element for it
     games.forEach((game) => {
       const game105 = document.createElement("div");
+      game105.className = "game-box";
       game105.innerHTML = `<!-- ${game.name} -->
       <div class="a">
       <div class="banner">
@@ -26,12 +27,19 @@ fetch("/assets/json/games2.json")
     </seb>
    </div>
    </div>`;
-      document.querySelector(".games").appendChild(game105);
-      // Add click event listener to th e game element to show the game in the game container
-      game105.onclick = (e) => {
-        width = screen.width;
-        height = screen.height;
-        var html = `<html>
+      /* <!-- ${game.name} -->
+      <div class="a">
+      <div class="banner">
+         <seb>
+            <a class="classText" href="${game.root}/" style="color:white;" target="_blank" rel="noopener noreferrer" onclick='console.log("Clicked on ${game.name}")'>
+            <div class="main">${game.name}</div>
+            <div class="sub">${game.sub}</div>
+            </a>
+    </seb>
+   </div>
+   </div>
+   */
+  /* <html>
 
 <head>
 <title> </title>
@@ -47,21 +55,157 @@ html, body{
 </body>
 
 </html>
-`;
+`*/
+   game105.onclick = (e) => {
+    let desca = '#'+game.id
+   $(desca).fadeIn(105);
+  // (game.name)
+  };
+      gamesContainer.appendChild(game105);
+      // Add click event listener to the game element to show the game in the game container
 
-        var aboutblank = window.open("", "Games");
-        aboutblank.document.write(html);
-      };
+      // Add click event listener to the back button in the game container to go back to the games list
+      gameNav.querySelector("#back").addEventListener("click", (e) => {});
+
+      // Add click event listener to the fullscreen button in the game container to enter fullscreen mode
+      gameNav.querySelector("#fullscreen").addEventListener("click", (e) => {
+        if (!document.fullscreenElement) {
+          gameFrame.requestFullscreen();
+        }
+      });
     });
   })
   .catch((e) => {
-    swal("Could not load games\n-Sebastian-105");
-    swal(e + "\n-Sebastian-105");
+    swal({
+      title: "Good job!",
+      text: "The error was " + e,
+      icon: "error",
+      button: "Continue",
+    });
   });
-
 
 // Hide the spinner element after the page is loaded
 document.querySelector(".spinner").style.display = "none";
+
+// Function to get the main save data
+function getMainSave() {
+  var mainSave = {};
+
+  // List of items in localStorage that should not be saved
+  var localStorageDontSave = ["theme", "tab", "nebelung"];
+
+  // Convert localStorage to an array of key-value pairs and remove the items that should not be saved
+  localStorageSave = Object.entries(localStorage);
+
+  for (let entry in localStorageSave) {
+    if (localStorageDontSave.includes(localStorageSave[entry][0])) {
+      localStorageSave.splice(entry, 1);
+    }
+  }
+
+  // Convert the localStorage array to a base64-encoded JSON string
+  localStorageSave = btoa(JSON.stringify(localStorageSave));
+
+  // Add the localStorage data to the mainSave object
+  mainSave.localStorage = localStorageSave;
+
+  // Get the cookies data and add it to the mainSave object
+  cookiesSave = document.cookie;
+  cookiesSave = btoa(cookiesSave);
+  mainSave.cookies = cookiesSave;
+
+  // Convert the mainSave object to a base64-encoded JSON string
+  mainSave = btoa(JSON.stringify(mainSave));
+
+  // Encrypt the mainSave data using AES encryption with the key 'save'
+  mainSave = CryptoJS.AES.encrypt(mainSave, "save").toString();
+
+  // Return the encrypted mainSave data
+  return mainSave;
+}
+
+// Function to download the main save data as a file
+function downloadMainSave() {
+  var data = new Blob([getMainSave()]);
+  var dataURL = URL.createObjectURL(data);
+
+  var fakeElement = document.createElement("a");
+  fakeElement.href = dataURL;
+  fakeElement.download = "games.save";
+  fakeElement.click();
+  URL.revokeObjectURL(dataURL);
+}
+
+// Function to get the main save data from an uploaded file
+function getMainSaveFromUpload(data) {
+  // Decrypt the uploaded data using AES decryption with the key 'save'
+  data = CryptoJS.AES.decrypt(data, "save").toString(CryptoJS.enc.Utf8);
+
+  // Parse the decrypted data as JSON
+  var mainSave = JSON.parse(atob(data));
+  var mainLocalStorageSave = JSON.parse(atob(mainSave.localStorage));
+  var cookiesSave = atob(mainSave.cookies);
+
+  // Set the items in localStorage using the uploaded data
+  for (let item in mainLocalStorageSave) {
+    localStorage.setItem(
+      mainLocalStorageSave[item][0],
+      mainLocalStorageSave[item][1]
+    );
+  }
+
+  // Set the cookies using the uploaded data
+  document.cookie = cookiesSave;
+}
+
+// Function to handle the file upload
+function uploadMainSave() {
+  var hiddenUpload = document.querySelector(".hiddenUpload");
+  hiddenUpload.click();
+
+  // Listen for the change event on the file input element
+  hiddenUpload.addEventListener("change", function (e) {
+    var files = e.target.files;
+    var file = files[0];
+    if (!file) {
+      return;
+    }
+
+    // Read the contents of the uploaded file as text and call getMainSaveFromUpload with the result
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      getMainSaveFromUpload(e.target.result);
+
+      // Show a success message to the user
+      var uploadResult = document.querySelector(".uploadResult");
+      uploadResult.innerText = "Uploaded save!";
+      uploadResult.style.display = "initial";
+      setTimeout(function () {
+        uploadResult.style.display = "none";
+      }, 3000);
+    };
+
+    reader.readAsText(file);
+  });
+}
+
+// Handle the hii pattern when keys are pressed
+var hiiPattern = ["h", "i", "i"];
+var hiiCurrent = 0;
+
+document.addEventListener("keydown", function (e) {
+  if (e.key !== hiiPattern[hiiCurrent]) {
+    return (hiiCurrent = 0);
+  }
+
+  hiiCurrent++;
+
+  if (hiiPattern.length == hiiCurrent) {
+    hiiCurrent = 0;
+    document.querySelector(".hii").removeAttribute("hidden");
+  }
+});
 //Shadow on scroll script
 let lastKnownScrollPosition = 0;
 let ticking = false;
