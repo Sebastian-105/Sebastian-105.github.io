c81909209659f896ba06e2044748164c2563a8e0
Seb_105 <28schapfel1@gmail.com>
2023-10-11 16:16:08 +0000

    asd

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
 const express = require('express');
+const http = require('http');
+
+
+// Constants
+const PORT = 1050;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
-const port = 3000;
+app.use(express.static('./'));
 
-app.get('/', (req, res) => {
-    res.send("a");
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Node.js is working\n- Sebastian-105`)
