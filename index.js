b53afd3e758777f7b8077a935c183091bef6d48a
Seb_105 <28schapfel1@gmail.com>
2023-10-18 12:08:49 -0400

    k

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-'use strict';
 
 const express = require('express');
 const http = require('http');
