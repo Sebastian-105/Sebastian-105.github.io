f07f52abef600eb957be477066c2437cfa084339
Seb_105 <28schapfel1@gmail.com>
2023-11-14 12:44:44 -0500

    a

--- app.js
+++ app.js
@@ -2,22 +2,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const router = express.Router();
+// const router = express.app();
 app.use(express.static(__dirname + "/"));
 // Setup essential routes
-router.get("/", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
-router.get("/assets/*", function (req, res) {
+app.get("/assets/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/401.html"));
 });
-router.get("/about", function (req, res) {
+app.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/105/about-me/index.html"));
 });
-router.get("/search-result", function (req, res) {
-  res.sendFile(path.join(__dirname + "/search/105.html"));
+app.get("/search-result", function (req, res) {
+  res.sendFile(path.join(__dirname + "/search/result/index.html"));
 });
-app.get('/hello', (req, res) => {
+app.get('/hello', function (req, res) {
   res.status(200).json({
     message: "Hello World!"
   });
