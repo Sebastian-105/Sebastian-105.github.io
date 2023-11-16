6138e66173af4e278047ff58dc08608687dafbed
Seb_105 <28schapfel1@gmail.com>
2023-11-16 15:45:59 +0000

    a

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const app = express();
 const path = require("path");
 
 // const router = express.app();
+// eslint-disable-next-line no-undef
 app.use(express.static(__dirname + "/"));
 // Setup essential routes
 app.get("/", function (req, res) {
+  // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 app.get("/assets/*", function (req, res) {
+  // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/401.html"));
 });
 app.get("/about", function (req, res) {
+// eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/105/about-me/index.html"));
 });
 app.get("/search-result", function (req, res) {
+  // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/search/result/index.html"));
 });
 app.get('/hello', function (req, res) {
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { app } = require('./app');
 
-const PORT = 8080;
+const PORT = 1050;
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
