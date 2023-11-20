c9cd868249e0c1c6f30af762c7abbcfd7808bddf
Seb_105 <28schapfel1@gmail.com>
2023-11-20 10:32:03 -0500

    s

--- app.js
+++ app.js
@@ -2,22 +2,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const routes = [
-  { path: '/', file: 'index.html' },
-  { path: '/assets/*', file: '401.html' },
-  { path: '/about', file: '105/about-me/index.html' },
-  { path: '/search-result', file: 'search/result/index.html' }
-];
-routes.forEach((route) => {
-  app.get(route.path, function (req, res) {
-    // eslint-disable-next-line no-undef
-    res.sendFile(path.join(__dirname + route.file));
-  });
-});
+
 // const router = express.app();
 // eslint-disable-next-line no-undef
 app.use(express.static(__dirname + "/"));
 // Setup essential routes
+app.get("/", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get("/assets/*", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/401.html"));
+});
+app.get("/about", function (req, res) {
+// eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/105/about-me/index.html"));
+});
+app.get("/search-result", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/search/result/index.html"));
+});
 app.get('/hello', function (req, res) {
   res.status(200).json({
     message: "Hello World!"
