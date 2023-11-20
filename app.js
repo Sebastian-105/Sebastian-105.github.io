e0496aff9879c23d3fe50c8be35168ec9affee5b
Seb_105 <28schapfel1@gmail.com>
2023-11-20 11:28:06 -0500

    a

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(__dirname + "/"));
+app.get("/", function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 const routes = [
-  { path: '/', file: 'index.html' },
   { path: '/assets/*', file: '/401.html' },
   { path: '/about', file: '/105/about-me/index.html' },
   { path: '/search-result', file: '/search/result/index.html' },
