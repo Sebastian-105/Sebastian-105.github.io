0e59523a1107d7248cc9ce5cbf2d5a64362725f1
Sebastian <sebastianschapfel105@gmail.com>
2024-01-09 12:50:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.get("/search-result", function (req, res) {
   // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/search/result/index.html"));
 });
-app.get("/assets/*", function (req, res) {
-  // eslint-disable-next-line no-undef
-  res.sendFile(path.join(__dirname + "/401.html"));
-});
 app.get('/hello1', function (req, res) {
   res.status(200).json({
     message: "Hello World!"
