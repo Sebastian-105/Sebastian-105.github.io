ddd546d656d80999447a5613c607452ea35a8253
Sebastian <sebastianschapfel105@gmail.com>
2024-03-07 14:15:41 +0000

    vercel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app } = require('./app');
 
 const PORT = 1050;
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
